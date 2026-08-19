c6dd498ec798d2b8071d21ec61dab4e0eb2f2267
kkagado70-cmd <kkagado70@gmail.com>
2026-08-19 19:04:04 -0300

    Update PreciseGuiScaleClient.java

--- src/main/java/com/example/PreciseGuiScaleClient.java
+++ src/main/java/com/example/PreciseGuiScaleClient.java
@@ -4,19 +4,22 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.glfw.GLFW;
 
 public class PreciseGuiScaleClient implements ClientModInitializer {
+    private static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(
+            ResourceLocation.fromNamespaceAndPath("preciseguiscale", "main")
+    );
+
     private static KeyMapping openGuiKey;
 
     @Override
     public void onInitializeClient() {
-        // Usa KeyMapping.Category.MISC em vez de String
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.preciseguiscale.open_gui",
                 GLFW.GLFW_KEY_RIGHT_SHIFT,
-                KeyMapping.Category.MISC
+                CATEGORY
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
