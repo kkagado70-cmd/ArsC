8da54612b06c073cf1cd850b7146561e7843c38a
kkagado70-cmd <kkagado70@gmail.com>
2026-08-22 00:55:58 -0300

    Fix KeyMapping category constant usage

--- src/main/java/com/example/XbowCart.java
+++ src/main/java/com/example/XbowCart.java
@@ -32,7 +32,7 @@ public class XbowCart implements ClientModInitializer {
             "key.xbowcart.trigger",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_X,
-            KeyMapping.Category.MISC
+            KeyMapping.CATEGORY_MISC
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
